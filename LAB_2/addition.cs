b394d347cc39bafe888fc4633e6961422336b38f
Spidy-crypto <63151411+Spidy-crypto@users.noreply.github.com>
2020-08-08 23:37:39 +0530

    Add files via upload

---
+++ LAB_2/addition.cs
@@ -0,0 +1,8 @@
+using System;
+namespace addition{
+	public class Addition{
+		static int addition(int num1,int num2){
+			return num1+num2;
+		}
+	}
+}
---
+++ LAB_2/multiplication.cs
@@ -0,0 +1,8 @@
+using System;
+namespace multiplication{
+	public class Multiplication{
+		static int multiplication(int num1,int num2){
+			return num1+num2;
+		}
+	}
+}
